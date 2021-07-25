89077e7fe4837cfd8013b32fd0ab783fb62c6349
Lucas Zalazar <lucas.zalazar6@gmail.com>
2021-07-25 01:00:45 -0300

    Se agregan comentarios y comando "make doc"  en makefile

--- inc/alumnos.h
+++ inc/alumnos.h
@@ -29,6 +29,17 @@ extern "C" {
 
 /*=====[Definitions of public data types]====================================*/
 
+/**
+ * @brief Estructura del tipo datos alumno_s con la información del alumno.
+ *
+ * Estructura del tipo de datos struct alumno_s que contiene los datos que 
+ * parametrizan a los alumnos.
+ *
+ * @param   alumno_t    puntero a estructura alumno_s que contiene la informacón del alumno.
+ * @param   apellidos   cadena de caracteres que contiene el apellido del alumno.
+ * @param   nombres     cadena de caracteres que contiene el nombre del alumno.
+ * @param   documento   cadena de caracteres que contiene el numero de documento del alumno.
+ */
 typedef struct alumno_s {
     char apellidos[30];
     char nombres[30];
@@ -39,8 +50,35 @@ typedef struct alumno_s {
 
 /*=====[Prototypes (declarations) of public functions]=======================*/
 
+/**
+ * @brief Función para serializar los datos de cada alumno.
+ *
+ * La función SerializarAlumno imprime por consola cada alumnos que 
+ * forma parte de la estructura del tipo alumno_s en un formato definido.
+ *
+ * @param   cadena      Cadena de caracteres a imprimir.
+ * @param   espacio     Tamaño de la cadena de caracteres a imprimir
+ * @param   alumno      Estructura que contiene el alumno a serializar.
+ * 
+ * @return  true        Se impirimeron correctamente los datos del alumno.
+ * @return  false       No se imprimen mas datos de alumnos.
+ */
 bool SerializarAlumno(char * cadena, size_t espacio, const alumno_t alumno);
 
+/**
+ * @brief Función para serializar todos los alumnos de la estructura ALUMNOS.
+ *
+ * La función SerializarAlumnos recorre los alumnos que contiene la estructura ALUMNOS.
+ * La funcíón verifica la disponibilidad de tamaño de la cadena de caracteres 
+ * que contiene los alumnos a imprimir.
+ * Los alumnos son impresos mediante el llamado de la función SerializarAlumno.
+ *
+ * @param   cadena      Cadena de caracteres a imprimir.
+ * @param   espacio     Tamaño de la cadena de caracteres a imprimir
+ * 
+ * @return  true        Se impirimeron correctamente todos los alumnos de la estructura.
+ * @return  false       No es posible imprimir la información de los alumnos.
+ */
 bool SerializarAlumnos(char * cadena, size_t espacio);
 
 /*=====[Prototypes (declarations) of public interrupt functions]=============*/
--- makefile
+++ makefile
@@ -5,6 +5,7 @@ BIN_DIR = ./build
 
 SRC_FILES = $(wildcard $(SRC_DIR)/*.c)
 OBJ_FILES = $(patsubst $(SRC_DIR)%.c,$(OBJ_DIR)%.o,$(SRC_FILES))
+DOC_FILES = ./Doxyfile
 
 all: build_folders $(OBJ_FILES)
 	@echo Building aplication
@@ -14,6 +15,10 @@ $(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
 	@echo Compiling source file $(notdir $(basename $<))
 	@gcc -c $< -I$(INC_DIR) -o $@
 
+.PHONY: doc
+doc:
+	@echo Building documentation to cese-ids-15co-tp5b
+	@doxygen $(DOC_FILES)
 ###############################################################################
 # Creation of output folders
 build_folders: $(BIN_DIR) $(LIB_DIR) $(OBJ_DIR) $(DOC_DIR) $(DPN_DIR) $(RST_DIR)
@@ -29,3 +34,4 @@ $(BIN_DIR): $(OUT_DIR)
 $(OBJ_DIR): $(OUT_DIR)
 	@echo Creating output objects folder
 	@mkdir $(OBJ_DIR)
+
